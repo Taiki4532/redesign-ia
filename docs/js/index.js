6e06be441a5db9bccbcfc2979bbbc15e20042231
Taiki Sekiguchi <ukurere3617@gmai.com>
2020-06-19 20:57:37 +0900

    教員の写真を表示するまで

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,6 +2,8 @@ const uri = 'https://script.google.com/macros/s/AKfycbxyacpN8y4nxSAnU0Eji6E_rBRD
 const id = '1BpGnuwC4lZf9G2yFyiSrxbJuGO8gviV8mr-I2D3x4vA';
 const sheet = 'studio';
 const endpoint = `${uri}?id=${id}&sheet=${sheet}`;
+const sheet2 = 'Faculty';
+const endpoint2 = `${uri}?id=${id}&sheet=${sheet2}`;
 
 const renderJson = (json) => {
   const studios = json.records;
@@ -12,14 +14,14 @@ const renderJson = (json) => {
    const studioPhoto = document.createElement("img");
    studioTitle.className = 'studio-title';
    studioTitle.textContent = studio['name-ja'];
-   studioPhoto.className = 'studio-photo'
-   studioPhoto.src = studio['photo1']
+   studioPhoto.className = 'studio-photo';
+   studioPhoto.src = studio['photo1'];
    studioDiv_ja.appendChild(studioPhoto);
    studioDiv_ja.appendChild(studioTitle);
    document.getElementById('studios').appendChild(studioDiv_ja);
 
  });
-  document.getElementById('result').textContent = JSON.stringify(json, null, 1);
+  //document.getElementById('result').textContent = JSON.stringify(json, null, 1);
 }
 
 const getData = async () => {
@@ -36,3 +38,48 @@ const getData = async () => {
   }
 }
 getData();
+
+const renderJson2 = (json) => {
+  const faculties = json.records;
+  faculties.forEach(faculty => {
+
+   const facultyDiv_ja = document.createElement('div');
+   const facultyName = document.createElement("span");
+   const facultyTitle = document.createElement("span");
+   const facultyStudio = document.createElement("span");
+   const facultyMajor = document.createElement("span");
+   const facultyPhoto = document.createElement("img");
+   facultyName.className = 'faculty-name';
+   facultyName.textContent = faculty['f-faculty-ja'];
+   facultyTitle.className = 'faculty-title';
+   facultyTitle.textContent = faculty['f-faculty-title-ja'];
+   facultyStudio.className = 'faculty-studio';
+   facultyStudio.textContent = faculty['f-studio-ja'];
+   facultyMajor.className = 'faculty-name';
+   facultyMajor.textContent = faculty['major-ja'];
+   facultyPhoto.className = 'faculty-photo';
+   facultyPhoto.src = faculty['faculty-photo'];
+   facultyDiv_ja.appendChild(facultyPhoto);
+   facultyDiv_ja.appendChild(facultyName);
+   facultyDiv_ja.appendChild(facultyTitle);
+   facultyDiv_ja.appendChild(facultyStudio);
+   facultyDiv_ja.appendChild(facultyMajor);
+   document.getElementById('faculties').appendChild(facultyDiv_ja);
+ });
+ //document.getElementById('result').textContent = JSON.stringify(json, null, 1);
+}
+
+const getData2 = async () => {
+  try{
+    const response2 =  await fetch(endpoint2);
+    if(response2.ok){
+      let jsonResponse2 = await response2.json();
+        jsonResponse2.records.pop();
+        renderJson2(jsonResponse2);
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+getData2();
