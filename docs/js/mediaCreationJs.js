68353624131bde338814a78f98cd81ccc9b8ffd5
Taiki Sekiguchi <ukurere3617@gmai.com>
2020-06-27 13:58:30 +0900

    add sidemenu in studio pages

--- docs/js/mediaCreationJs.js
+++ docs/js/mediaCreationJs.js
@@ -229,3 +229,14 @@ const getData = async () => {
 }
 
 getData();
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementsById("openBtn").style.visibility = "hidden";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementsById("openBtn").style.visibility = "visible";
+}
